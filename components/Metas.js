97f059020b84446adab7e5eba5acb4348d376264
Doly Jimenez <doly.jimenez11@gmail.com>
2021-09-01 18:36:56 -0500

    campos nombre y descripcion

--- components/Metas.js
+++ components/Metas.js
@@ -1,10 +1,65 @@
 import React from 'react'
-import { Text } from 'react-native';
+import { Text, StyleSheet, View, TextInput } from 'react-native';
 
 const Metas = () => {
     return(
-        <Text> Metas </Text>
+        <View style={styles.contenedor}>
+            <Text style={styles.titulo}> Crear Meta </Text>
+
+            <View style={styles.formulario}>
+                <Text style={styles.subtitulos}>Nombre:</Text>
+                <TextInput style={styles.input}/>
+
+                <Text style={styles.subtitulos}>Descripción:</Text>
+                <TextInput
+                style={styles.textArea}
+                />
+
+
+
+            </View>
+
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    
+    contenedor: {
+        margin: 20
+    },
+
+    formulario:{
+        marginHorizontal: '2.5%'
+    },
+
+    titulo: {        
+        textTransform: 'uppercase',
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    
+    subtitulos:{
+        marginTop: 5,
+        fontSize:14
+    },
+
+    input:{
+        height: 40,
+        marginVertical:5,
+        borderWidth:1, 
+        borderStyle: 'solid',
+        borderColor: '#000'
+
+    },
+
+    textArea:{
+        height: 80,
+        marginVertical:5,
+        borderWidth:1, 
+        borderStyle: 'solid',
+        borderColor: '#000'
+    }
+});
 
 export default Metas;
