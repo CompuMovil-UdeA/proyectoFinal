04295924dd2a11367f89578fa23da3bbadb1568b
Doly Jimenez <doly.jimenez11@gmail.com>
2021-09-01 20:23:36 -0500

    instalacion de dependencia para fecha, se incluyen campos de fecha inicial y final

--- components/Metas.js
+++ components/Metas.js
@@ -1,64 +1,142 @@
-import React from 'react'
-import { Text, StyleSheet, View, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import {
+    Text,
+    StyleSheet,
+    View,
+    TextInput,
+    Button,
+    TouchableHighlight,
+} from 'react-native';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
 const Metas = () => {
-    return(
+    const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+    /* Fecha Inicial*/
+    const showDatePicker = () => {
+        setDatePickerVisibility(true);
+    };
+
+    const hideDatePicker = () => {
+        setDatePickerVisibility(false);
+    };
+
+    const handleConfirm = date => {
+        const opciones = { year: 'numeric', month: 'long', day: '2-digit' };
+        guardarFechaInicio(date.toLocaleDateString('es-ES', opciones));
+        hideDatePicker();
+    };
+
+    /* Fecha Final*/
+    const showDatePickerFinal = () => {
+        setDatePickerVisibility(true);
+    };
+
+    const hideDatePickerFinal = () => {
+        setDatePickerVisibility(false);
+    };
+
+    const handleConfirmDateFinal = date => {
+        const opciones = { year: 'numeric', month: 'long', day: '2-digit' };
+        guardarFechaFinal(date.toLocaleDateString('es-ES', opciones));
+        hideDatePickerFinal();
+    };
+
+    const [fechaInicio, guardarFechaInicio] = useState('');
+    const [fechaFinal, guardarFechaFinal] = useState('');
+
+    return (
         <View style={styles.contenedor}>
             <Text style={styles.titulo}> Crear Meta </Text>
 
             <View style={styles.formulario}>
                 <Text style={styles.subtitulos}>Nombre:</Text>
-                <TextInput style={styles.input}/>
+                <TextInput style={styles.input} />
 
                 <Text style={styles.subtitulos}>Descripción:</Text>
-                <TextInput
-                style={styles.textArea}
-                />
+                <TextInput style={styles.textArea} />
 
+                <View>
+                    <Text>Fecha de inicio:</Text>
+                    <Text>{fechaInicio}</Text>
+                    <Button title="Seleccionar fecha" onPress={showDatePicker} />
+                    <DateTimePickerModal
+                        isVisible={isDatePickerVisible}
+                        mode="date"
+                        onConfirm={handleConfirm}
+                        onCancel={hideDatePicker}
+                    />
+                </View>
 
+                <View>
+                    <Text>Fecha final:</Text>
+                    <Text>{fechaFinal}</Text>
+                    <Button title="Seleccionar fecha" onPress={showDatePickerFinal} />
+                    <DateTimePickerModal
+                        isVisible={isDatePickerVisible}
+                        mode="date"
+                        onConfirm={handleConfirmDateFinal}
+                        onCancel={hideDatePickerFinal}
+                    />
+                </View>
+
+                <TouchableHighlight style={styles.btnCrear}>
+                    <Text style={styles.textoCrear}>Agregar Meta</Text>
+                </TouchableHighlight>
 
             </View>
-
         </View>
     );
-}
+};
 
 const styles = StyleSheet.create({
-    
     contenedor: {
-        margin: 20
+        margin: 20,
     },
 
-    formulario:{
-        marginHorizontal: '2.5%'
+    formulario: {
+        marginHorizontal: '2.5%',
     },
 
-    titulo: {        
+    titulo: {
         textTransform: 'uppercase',
         fontSize: 18,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
     },
-    
-    subtitulos:{
+
+    subtitulos: {
         marginTop: 5,
-        fontSize:14
+        fontSize: 14,
     },
 
-    input:{
+    input: {
         height: 40,
-        marginVertical:5,
-        borderWidth:1, 
+        marginVertical: 5,
+        borderWidth: 1,
         borderStyle: 'solid',
-        borderColor: '#000'
-
+        borderColor: '#000',
     },
 
-    textArea:{
+    textArea: {
         height: 80,
-        marginVertical:5,
-        borderWidth:1, 
+        marginVertical: 5,
+        borderWidth: 1,
         borderStyle: 'solid',
-        borderColor: '#000'
+        borderColor: '#000',
+    },
+
+    btnCrear: {
+        backgroundColor: '#4483FA',
+        padding: 10,
+        marginTop: '40%',
+        marginBottom: '20%'
+    },
+
+    textoCrear: {
+        color: '#fff',
+        textTransform: 'uppercase',
+        textAlign: 'center',
+        fontWeight: 'bold'
     }
 });
 
